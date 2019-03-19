3c6933e072553a5402aa57ba60297c9744a518dc
Администратор <shalabodov.roman@mail.ru>
2019-03-19 13:56:20 +0300

    feat: add class TranspositionList

--- object-oriented-programming/src/main/java/com/netcracker/courses/oop/Main.java
+++ object-oriented-programming/src/main/java/com/netcracker/courses/oop/Main.java
@@ -1,6 +1,16 @@
 package com.netcracker.courses.oop;
+import com.netcracker.courses.methods.Group;
+import com.netcracker.courses.methods.Sort;
+import com.netcracker.courses.methods.TranspositionList;
+import com.netcracker.courses.triangles.Triangle;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        List<Triangle> list=new ArrayList(Group.createGroup());
+        Group.show(list);
+        TranspositionList.transposition(list);
+        Group.show(Sort.sortOfSide(list));
     }
 }
